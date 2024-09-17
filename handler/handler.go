3b44ca69d1a7f968e02647bd59ccd13178606a21
Lucas Lima <lucasgabrieljake@gmail.com>
2024-09-17 10:33:46 -0300

    feat: update opening

--- handler/handler.go
+++ handler/handler.go
@@ -70,7 +70,59 @@ func ShowOpeningHandler(ctx *gin.Context) {
 }
 
 func UpdateOpeningHandler(ctx *gin.Context) {
+	request := CreateOpeningRequest{}
+
+	ctx.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("Validation Error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	logger.Infof("%+v", request)
+
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequired("id", "query").Error())
+		return
+	}
 	
+	opening := schemas.Opening{}
+	query:= db.First(&opening, id)
+
+	if(query.Error != nil){
+		sendError(ctx, http.StatusBadRequest, fmt.Sprintf("opening with id %s is not found", id))
+		return
+	}
+	
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+
+	update := db.Save(&opening)
+	if update.Error != nil {
+		logger.Errorf("error updating opening: %v", update.Error.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+	sendSuccess(ctx, "update-opening", opening)
+
 }
 
 func DeleteOpeningHandler(ctx *gin.Context) {
